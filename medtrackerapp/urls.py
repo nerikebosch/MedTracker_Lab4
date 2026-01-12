df5d6b956cc1d292872fa14197c534f262322c25
nerikeb <nerike.b@gmail.com>
2026-01-12 16:16:54 +0100

    Added part 1 features

--- medtrackerapp/urls.py
+++ medtrackerapp/urls.py
@@ -2,6 +2,22 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MedicationViewSet, DoseLogViewSet, NoteViewSet
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Software engineering lab",
+        default_version="v1",
+        description="API documentation for the lab",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    authentication_classes=[],
+)
+
 router = DefaultRouter()
 router.register("medications", MedicationViewSet, basename="medication")
 router.register("logs", DoseLogViewSet, basename="doselog")
@@ -9,4 +25,6 @@ router.register("notes", NoteViewSet, basename="note")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger',
+    cache_timeout=0), name='schema-swagger-ui'),
 ]
